653951f7b6c4b0a41c007f71aac5004e79a18f26
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-04-29 14:10:07 -0300

    Implemented reminder creation route

--- lib/routes/route_create_reminder.dart
+++ lib/routes/route_create_reminder.dart
@@ -1,11 +1,77 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_training/models/Reminder.dart';
+import 'package:flutter_training/widgets/widget_mandatory_input.dart';
 
 class CreateReminderRoute extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) => Center(
-    child: Text("Create reminder"),
-  );
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Create new reminder"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: CreateReminderForm(),
+        )
+    );
+  }
+
+}
+
+class CreateReminderForm extends StatelessWidget {
+
+  final _reminderForm = GlobalKey<FormState>();
+
+  final title = TextEditingController();
+  final description = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _reminderForm,
+      child: Column(
+        children: [
+          MandatoryInput(title, "Title"),
+          MandatoryInput(description, "Description"),
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () { _createReminder(context); },
+                      child: Text("Create reminder",
+                        style: TextStyle(color: Colors.white)
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _createReminder(BuildContext context) {
+    if (_isValidInput()) {
+      _submitReminder(context);
+    }
+  }
+
+  bool _isValidInput() {
+    return _reminderForm.currentState.validate();
+  }
+
+  void _submitReminder(BuildContext context) {
+    Navigator.of(context).pop(Reminder(title.text, description.text));
+  }
 
 }
