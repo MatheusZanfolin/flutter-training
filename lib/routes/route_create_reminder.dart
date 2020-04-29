27e49c20c2b61da3a8e747f262b225740ff4e22d
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-04-29 14:03:53 -0300

    Added reminder upgrade structure

---
+++ lib/routes/route_create_reminder.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
+
+class CreateReminderRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) => Center(
+    child: Text("Create reminder"),
+  );
+
+}
--- lib/routes/route_main.dart
+++ lib/routes/route_main.dart
@@ -1,15 +1,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_training/models/Reminder.dart';
+import 'package:flutter_training/routes/route_create_reminder.dart';
 
 class MainRoute extends StatefulWidget {
+
   MainRoute({Key key}) : super(key: key);
 
   @override
   _MainRouteState createState() => _MainRouteState();
+
 }
 
 class _MainRouteState extends State<MainRoute> {
+
+  Reminder reminder = Reminder("Title", "Description");
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +24,32 @@ class _MainRouteState extends State<MainRoute> {
         title: Text("Reminders"),
       ),
       body: Center(
-        child: Text("Hello, world!"),
-      )
+        child: Column(
+          children: [
+            Text(reminder.title),
+            Text(reminder.description)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _createReminder,
+        child: Icon(Icons.add),
+      ),
     );
   }
+
+  void _createReminder() {
+    _makeReminder().then(_addReminder);
+  }
+
+  Future<Reminder> _makeReminder() {
+    return Navigator.push(context, MaterialPageRoute(builder: (context) => CreateReminderRoute()));
+  }
+
+  void _addReminder(Reminder reminder) => setState(() {
+    if (reminder != null) {
+      setState(() { this.reminder = reminder; });
+    }
+  });
 
 }
