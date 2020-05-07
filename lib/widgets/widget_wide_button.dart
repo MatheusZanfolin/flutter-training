7bba62e0173307b4ddc40fa30004197f17d7b003
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-05-07 16:35:30 -0300

    Created 'contracts' directory

--- lib/widgets/widget_wide_button.dart
+++ lib/widgets/widget_wide_button.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_training/models/ButtonStyle.dart';
+import 'package:flutter_training/contracts/ButtonStyle.dart';
 
 class WideButton extends StatelessWidget {
 
