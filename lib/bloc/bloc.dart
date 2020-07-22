685877ffabb2913bc540fd3f72a283c1f78f5b51
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-07-22 11:20:44 -0300

    Created BlocProvider widget

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -15,3 +15,32 @@ abstract class BLoC<T> {
   void dispose() => _controller.close();
 
 }
+
+class BlocProvider<T extends BLoC> extends StatefulWidget {
+
+  final Widget child;
+  final T bloc;
+
+  const BlocProvider({Key key, @required this.bloc, @required this.child}) : super(key: key);
+
+  static T of<T extends BLoC>(BuildContext context) {
+    return context.findAncestorWidgetOfExactType<BlocProvider<T>>().bloc;
+  }
+
+  @override
+  State createState() => _BlocProviderState();
+  
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+}
