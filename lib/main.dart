cd4dc9f1641c5816066f89cc836f3cc0ab53ba66
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2020-07-22 12:02:49 -0300

    Added top level provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_training/bloc/bloc.dart';
+import 'package:flutter_training/bloc/blocs/bloc_reminders.dart';
 import 'package:flutter_training/routes/route_main.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => BlocProvider<RemindersBloc>(
+    bloc: RemindersBloc(),
+    child: MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MainRoute(),
-    );
-  }
+    ),
+  );
 }
